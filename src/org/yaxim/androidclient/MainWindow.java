caf94b78163642509ddb08fde6ec8ac95dc17007
Georg Lukas <georg@op-co.de>
2011-09-28 15:20:57 +0200

    presence notification translation, import

--- src/org/yaxim/androidclient/MainWindow.java
+++ src/org/yaxim/androidclient/MainWindow.java
@@ -44,6 +44,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.CheckBox;
 import android.widget.EditText;
